a4ab5bad4ca169e1e67d65dcd7cd92efced8de6f
RobertoValdezV13 <44959719+RobertoValdezV13@users.noreply.github.com>
2018-12-09 23:13:05 -0700

    Add files via upload
    profe las practicas las subi solo el archivo java porque no me deja mandarlas completas por que son muchos archivos estan el orden

---
+++ Principal.java
@@ -0,0 +1,46 @@
+﻿
+import java.util.Scanner;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ *    Roberto Valdez Vazquez 18550731
+ */
+public class Principal {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+
+
+        Scanner input = new Scanner (System.in);
+        int num;
+
+        int cont=2;
+        boolean primo=true;
+        
+        System.out.println("Introduzca un número entero Positivo");
+            num=input.nextInt();
+            
+           while((primo)&&(cont<(num/2))){
+                cont++;
+
+                if((num%cont)==0){
+                   primo=false;
+               }
+            }
+            if(primo){
+                System.out.println(num+" Tu numero es un número primo");
+            }else{
+                System.out.println(num+" Tu numero no es un número primo");
+            }
+    }
+    
+}
---
+++ RobClase.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author temporal2
+ */
+public class RobClase {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //LLAMADA DE FUNCION
+       RobClase mObjeto = new RobClase();
+        mObjeto.imprimemensaje("Roberto");
+    Math.random();
+    
+    }
+         void imprimemensaje (String mensaje){
+       System.out.println("Hola " + mensaje); 
+       }
+    //PARADIGMAS DE PROGRAMACION
+    //PROGRAMACION ESTRUCTURADA
+    //ETC ETC ETC
+    //nivel de acesso valor de retorno nombre de la funcion(argumentos) 
+    //cuerpo de la funcion
+    //op
+    //publico privado protegido 
+    //funciones regresan valores
+    //procedimientos no regresan valores
+   //void imprimemensaje (String mensaje){
+     //  System.out.println("Hola " +mensaje);     
+       
+    }
